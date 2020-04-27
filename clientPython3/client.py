584d34774be48bd07bbb26ad245bac9333b25681
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2020-04-27 12:00:02 +0530

    update buffer size

--- clientPython3/client.py
+++ clientPython3/client.py
@@ -351,7 +351,7 @@ class Application(tkinter.Tk):
     def clientchat(self):
         while not self.should_quit:     #If we are not in the 'quit' state then do :
             try:
-                data = self.conn.recv(10000000) #Receive and decode the data
+                data = self.conn.recv(1000000000) #Receive and decode the data
                 data = data.decode()
                 data = data.rstrip()
 
@@ -424,7 +424,7 @@ class Application(tkinter.Tk):
         while 1:
             if not got_list:
                 try:
-                    data = self.conn.recv(10000000) #Obtaining the list of all the active users
+                    data = self.conn.recv(1000000000) #Obtaining the list of all the active users
                     data = data.decode()
                     data = data.rstrip()
                 except:
@@ -438,7 +438,7 @@ class Application(tkinter.Tk):
                         self.conn.close()
                         self.client_menu()    
                     try:
-                        list_of_active_user = self.conn.recv(10000000).decode() #this will be a string of name separated by spaces
+                        list_of_active_user = self.conn.recv(1000000000).decode() #this will be a string of name separated by spaces
                     except:
                         self.conn.close()
                         self.client_menu() 
